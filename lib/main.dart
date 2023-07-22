b33eb8db0491c0d016c0c82c31d761dc2c4d704a
axiy1218 <122154635+axiy1218@users.noreply.github.com>
2023-07-22 19:25:59 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:todo_app_getx/app.dart';
+import 'package:todo_app_getx/utils/app_setup/app_setup.dart';
+
+/*
+Created by Axmadjon Isaqov on 09:13:31 21.11.2022
+© 2022 @axi_dev 
+*/
+/*
+Theme:::Todo App getx
+*/
+Future<void> main() async {
+  await AppSetup.setup;
+  runApp(DevicePreview(enabled: false, builder: (context) => const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+        minTextAdapt: true,
+        splitScreenMode: true,
+        designSize: const Size(390, 844),
+        builder: (context, child) => const AppProvider());
+  }
+}
